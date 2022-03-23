2b56731786cc1c26eb9abe4ac23a45cba463e23c
ooepi <robert.harmaakivi@gmail.com>
2022-03-23 10:43:22 +0200

    Files now in remote git

--- Ticket.java
+++ Ticket.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Data from IoT Ticket");
+        System.out.println("Data from IoT Ticket now in git.vamk.fi");
     }
 }
